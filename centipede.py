0fff64602ff67debf20c39d7495aee816ea894de
Poop Lord <tylerbarrett2010@gmail.com>
2019-11-03 19:41:14 -0500

    Modified centipede.py - if a certain limb specifies a desired log level, allow it to override the GENERAL log level.

--- centipede.py
+++ centipede.py
@@ -4,10 +4,10 @@ Centipede.py - top-level framework that instantiates and calls the limbs in orde
 
 import sys
 
-from centipede import resource_generator
-from centipede import text_notification_manager
-from centipede.package import Package
-from centipede import centipede_logger
+import resource_generator
+#import text_notification_manager
+from package import Package
+import centipede_logger
 
 
 class Centipede(object):
@@ -31,12 +31,15 @@ class Centipede(object):
             class_name = str(limb.__name__)
             sys.stdout.flush()
             specific_config = getattr(self.config, class_name)
-            specific_config["logger"] = centipede_logger.create_logger(class_name, self.log_level)
+            if ("log_level" in specific_config):
+                specific_config["logger"] = centipede_logger.create_logger(class_name, specific_config["log_level"])
+            else:
+                specific_config["logger"] = centipede_logger.create_logger(class_name, self.log_level)
             in_config = {**common_config, **specific_config}
             self.limbs.append(limb(in_config))
 
 
-    @text_notification_manager.text_alert_on_exception
+#   @text_notification_manager.text_alert_on_exception
     def walk(self):
         for page in self.page_generator.iterate_pages():
             package = Package()
